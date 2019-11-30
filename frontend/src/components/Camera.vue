dc09783b84c4a3ed9c17b6d1581cf9919afbd2c2
GurvirDehal <gurvirdehal@gmail.com>
2019-11-30 13:19:55 -0700

    Added front-end

---
+++ frontend/src/components/Camera.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+.camera
+  video#video(ref="video", width="640", height="480", autoplay)
+  .button(@click="photoSnapped")
+    p Snap Photo
+  canvas#canvas(ref="canvas", v-show="snapped", width="640", height="480")
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      snapped: false,
+      context: this.$refs.canvas.getContext('2d')
+    }
+  },
+  methods: {
+    photoSnapped() {
+      this.snapped = true
+      this.context.drawImage(this.$refs.video, 0, 0, 640, 480)
+    }
+  },
+
+  mounted() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.$refs.video.srcObject = stream
+        this.$refs.video.play()
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.camera {
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+}
+.button {
+  width: 120px;
+  height : 50px;
+  background-color: lightskyblue;
+  border-radius: 5px;
+  align-self: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+}
+#video {
+    align-self: center;
+}
+#canvas {
+    width: 640px;
+    height: 480px;
+    align-self: center;
+}
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.hello
+  h1 {{ msg }}
+  .button(v-if="!showCamera", @click="buttonClicked")
+    p Click Me!
+  camera(v-if="showCamera")
+</template>
+
+<script>
+// eslint-disable 
+import Camera from './Camera'
+export default {
+  name: 'HelloWorld',
+  components: {
+    Camera
+  },
+  data() {
+    return {
+      msg: 'Insert Title Here',
+      showCamera: false
+    }
+  },
+  methods: {
+    buttonClicked() {
+      this.showCamera = true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: 120px;
+  height : 50px;
+  background-color: lightskyblue;
+  border-radius: 5px;
+  align-self: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+.hello {
+  display: flex;
+  flex-direction: column;
+}
+</style>
